c87f24c04721b425acc7c23a7cd93be7e1e5cfff
Eric Scuccimarra <skooch@gmail.com>
2018-11-26 15:34:57 +0100

    Bugs

--- models.py
+++ models.py
@@ -61,7 +61,7 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = models.TextField()
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    parent_comment = models.ForeignKey("self", null=True, blank=True)
+    parent_comment = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
